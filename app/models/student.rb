86852671006506a589e7fee457392582d50c7104
Jonathan Luk <jluk87@gmail.com>
2019-03-16 04:03:42 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,17 +3,4 @@ class Student < ActiveRecord::Base
     self.first_name + " " + self.last_name
   end
 
-  def index
-
-  end
-
-  def new
-
-  end
-
-  def create
-    Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to students_path
-  end
-
 end
